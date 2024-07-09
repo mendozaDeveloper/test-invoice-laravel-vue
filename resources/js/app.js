9347d82bb57151c7e8320b4e9bb63bf9669dcf7b
LuisMendoza33 <lmendozaguerra@gmail.com>
2024-07-08 19:08:32 -0500

    puesta en marcha

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import app from './components/app.vue'
+import router from './components/router/index.js'
+
+createApp(app).use(router).mount('#app')
---
+++ resources/js/components/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from "vue-router";
+import invoiceIndex from '../invoices/index.vue'
+import invoiceNew from '../invoices/new.vue'
+import invoiceShow from '../invoices/show.vue'
+import invoiceEdit from '../invoices/edit.vue'
+import notFound from "../NotFound.vue";
+
+const routes = [
+    {
+        path: '/',
+        component: invoiceIndex
+    },
+    {
+        path: '/invoice/new',
+        component: invoiceNew
+    },
+    {
+        path: '/invoice/show/:id',
+        component: invoiceShow,
+        props: true
+    },
+    {
+        path: '/invoice/edit/:id',
+        component: invoiceEdit,
+        props: true
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: notFound
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
